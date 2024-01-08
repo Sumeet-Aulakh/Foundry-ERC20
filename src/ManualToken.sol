691801180f7d20186fa4d7866b4c75de17d6d154
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2024-01-08 09:22:02 -0600

    Add functionality to ManualToken contract

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -2,4 +2,29 @@
 
 pragma solidity 0.8.18;
 
-contract ManualToken {}
+contract ManualToken {
+    mapping(address => uint256) private s_balances;
+
+    function name() public pure returns (string memory) {
+        return "Manual Token";
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether; // 100 * 10^18
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return s_balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public {
+        uint256 previousBalances = s_balances[msg.sender] + s_balances[_to];
+        s_balances[msg.sender] -= _value;
+        s_balances[_to] += _value;
+        assert(s_balances[msg.sender] + s_balances[_to] == previousBalances);
+    }
+}
